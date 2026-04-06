51d963bb1e5aca2dd26cd4b76037f15df6db5226
The Guitar <turanlizad776@gmail.com>
2026-04-06 10:42:36 +0400

    a2

--- demoC#/arrays.cs
+++ demoC#/arrays.cs
@@ -1,1 +1,16 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("How many users do you want?");
+int numberOfUsers = int.Parse(Console.ReadLine());
+
+string[] names = new string[numberOfUsers];
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine("Enter username:");
+    names[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Users : ");
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"User{i} : {names[i]}");
+}
